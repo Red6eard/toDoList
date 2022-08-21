f6e84876e1b627d02e6407293ccb91a812a1ec77
Red6eard <101076866+Red6eard@users.noreply.github.com>
2022-08-21 05:04:53 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const inp = document.getElementById("inp");
+const button = document.getElementById("btn");
+const list = document.getElementById("list");
+
+function sendItem(btn){
+	console.log(button)
+}
+
+sendItem();
