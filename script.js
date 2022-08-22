9ea4d1322825a339061dcd832410bf57904a2deb
Red6eard <101076866+Red6eard@users.noreply.github.com>
2022-08-22 17:38:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+// поле для ввода
 const inp = document.getElementById("inp");
+
+// кнопка добавления пункта
 const button = document.getElementById("btn");
+
+//список
 const list = document.getElementById("list");
 
-function sendItem(btn){
-	
-}
 
+//при нажатии на кнопку происходит следующее:
+//вызывается функция sendItem,которая
+function sendItem(btn){
+  //создает пункт списка newList
+  const newList = document.createElement("li");
+  list.appendChild(newList,);
+  
+  //присваевает пункту списка значение введеное в поле ввода
+  newList.innerHTML = inp.value;
+  
+  //создает кнопку удаления пункта списка
+  const check =   document.createElement("button");
+  check.innerHTML = "OK";
+  newList.appendChild(check);
+  
+  //делает поле для ввода чистым 
+  inp.value = "";
+  
+};
